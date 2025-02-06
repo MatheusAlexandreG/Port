fbdb41eea4b51bc88ff8c216204adfa6e4ec04ca
matheus Oliveira <101216797+leadsolutions-devtwo@users.noreply.github.com>
2025-02-06 17:23:30 -0300

    update 1

--- meu-portifolio/resources/js/app.js
+++ meu-portifolio/resources/js/app.js
@@ -44,3 +44,20 @@ document.addEventListener("DOMContentLoaded", function () {
         carousel.scrollLeft = scrollLeft - walk;
     });
 });
+
+$(document).ready(function () {
+    $(".header-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        items: 1,
+        navText: [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+});
--- meu-portifolio/resources/views/components/app-layout.blade.php
+++ meu-portifolio/resources/views/components/app-layout.blade.php
@@ -14,6 +14,19 @@
 
     {{-- Google Font --}}
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap" rel="stylesheet">
+    
+    {{-- Bibliotecas adicionais --}}
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    {{-- Swiper CSS --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+    {{-- Owl Carousel CSS --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
 
     {{-- Importação do CSS e JS pelo Vite --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -22,34 +35,40 @@
 <body>
 
     {{-- Navbar Fixa --}}
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href="#">Mouri</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="{{ route('home') }}">Home</a>
-                    </li>
+    <nav class="navbar navbar-expand-lg navbar fixed-top">
+        <div class="container d-flex justify-content-between align-items-center">
+            <!-- Logo à esquerda -->
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('images/logo.gif') }}" alt="Logo Animada" width="100">
+            </a>
+            <!-- Centralização do menu -->
+            <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('about') }}">About</a>
+                        <a class="nav-link" href="{{ route('about') }}">Sobre</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('projects') }}">Portfolio</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('services') }}">Services</a>
+                        <a class="nav-link" href="{{ route('services') }}">Serviços</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('contact') }}">Contact</a>
+                        <a class="nav-link" href="{{ route('contact') }}">Contato</a>
                     </li>
                 </ul>
             </div>
+    
+            <!-- Botão à direita -->
+            <a href="" class="btn btn-primary">Get Started</a>
+    
+            <!-- Botão Hamburguer (Visível apenas em telas pequenas) -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                <span class="navbar-toggler-icon"></span>
+            </button>
         </div>
     </nav>
+    
 
     {{-- Conteúdo Dinâmico das Páginas --}}
     <main class="container mt-5 pt-5">
@@ -61,9 +80,17 @@
         <a href="#" target="_blank">Click for More</a>
     </div>
 
-    {{-- jQuery e Bootstrap JS --}}
+    {{-- jQuery (necessário para Owl Carousel e Bootstrap) --}}
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+    {{-- Bootstrap JS --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    {{-- Swiper JS --}}
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+    {{-- Owl Carousel JS --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 
 </body>
 </html>
